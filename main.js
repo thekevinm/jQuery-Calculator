5c0b96197bed6649629f5552659abfb95ee5bdcd
Kevin McGahey <kevinmcgahey1114@gmail.com>
2018-03-15 20:46:23 -0700

    done

--- main.js
+++ main.js
@@ -1,13 +1,14 @@
 $(document).ready(function() {
 	$(".button").click(function(){
-		document.getElementById(".screen").value += this.value;
+		document.getElementById("screen").value += this.value;
 	});
 	var result = 0;
 	var prevEntry = 0;
 	var currentEntry = '0';
+	var operation = null;
 	updateScreen(result);
 
-	$('#button').on('click', function(evt){
+	$('.button').on('click', function(evt){
 		var buttonPressed = $(this).html();
 		console.log(buttonPressed);
 
@@ -17,6 +18,30 @@ $(document).ready(function() {
 		} else if (buttonPressed === "1"){
 			result = 0;
 			currentEntry = '1';
+		} else if (buttonPressed === "2"){
+			result = 0;
+			currentEntry = '2';
+		} else if (buttonPressed === "3"){
+			result = 0;
+			currentEntry = '3';
+		} else if (buttonPressed === "4"){
+			result = 0;
+			currentEntry = '4';
+		} else if (buttonPressed === "5"){
+			result = 0;
+			currentEntry = '5';
+		} else if (buttonPressed === "6"){
+			result = 0;
+			currentEntry = '6';
+		} else if (buttonPressed === "7"){
+			result = 0;
+			currentEntry = '7';
+		} else if (buttonPressed === "8"){
+			result = 0;
+			currentEntry = '8';
+		} else if (buttonPressed === "9"){
+			result = 0;
+			currentEntry = '9';
 		} else if (buttonPressed === '.') {
 			currentEntry += '.'
 		} else if (isNumber(buttonPressed)) {
@@ -27,7 +52,8 @@ $(document).ready(function() {
 			operation = buttonPressed;
 			currentEntry = '';
 		} else if (buttonPressed === '=') {
-			currentEntry = operate(prevEntry, currentEntry);
+			currentEntry = operate(prevEntry, currentEntry, operation);
+			operation = null;
 		}
 		updateScreen(currentEntry);
 
@@ -36,25 +62,25 @@ $(document).ready(function() {
 
 updateScreen = function(displayValue) {
 	var displayValue = displayValue.toString();
-	$('.screen').html(displayValue.substring(0, 10));
+	$('#screen').html(displayValue.substring(0, 10));
 };
 
 isNumber = function(value) {
-	return isNaN(value);
+	return !isNaN(value);
 }
 
 isOperator = function(value) {
 	return value === '/' || value === '*' || value === '+' || value === '-';
 };
 
-operate = function(a, b) {
+operate = function(a, b, operation) {
 	a = parseFloat(a);
 	b = parseFloat(b);
-	console.log(a, b);
-	if (operate === '+') return a + b;
-	if (operate === '-') return a - b;
-	if (operate === '/') return a / b;
-	if (operate === '*') return a * b;
+	console.log(a, b, operation);
+	if (operation === '+') return a + b;
+	if (operation === '-') return a - b;
+	if (operation === '/') return a / b;
+	if (operation === '*') return a * b;
 };
 
 
