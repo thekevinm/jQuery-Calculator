1f6757adad58db79dfab8ca3f2b6af79fdfeb46a
Kevin McGahey <kevinmcgahey1114@gmail.com>
2018-03-17 12:21:38 -0700

    minor changes

--- main.js
+++ main.js
@@ -15,33 +15,33 @@ $(document).ready(function() {
 		if (buttonPressed === "C") {
 			result = 0;
 			currentEntry = '0';
-		} else if (buttonPressed === "1"){
-			result = 0;
-			currentEntry = '1';
-		} else if (buttonPressed === "2"){
-			result = 0;
-			currentEntry = '2';
-		} else if (buttonPressed === "3"){
-			result = 0;
-			currentEntry = '3';
-		} else if (buttonPressed === "4"){
-			result = 0;
-			currentEntry = '4';
-		} else if (buttonPressed === "5"){
-			result = 0;
-			currentEntry = '5';
-		} else if (buttonPressed === "6"){
-			result = 0;
-			currentEntry = '6';
-		} else if (buttonPressed === "7"){
-			result = 0;
-			currentEntry = '7';
-		} else if (buttonPressed === "8"){
-			result = 0;
-			currentEntry = '8';
-		} else if (buttonPressed === "9"){
-			result = 0;
-			currentEntry = '9';
+		// } else if (buttonPressed === "1"){
+		// 	result = 0;
+		// 	currentEntry = '1';
+		// } else if (buttonPressed === "2"){
+		// 	result = 0;
+		// 	currentEntry = '2';
+		// } else if (buttonPressed === "3"){
+		// 	result = 0;
+		// 	currentEntry = '3';
+		// } else if (buttonPressed === "4"){
+		// 	result = 0;
+		// 	currentEntry = '4';
+		// } else if (buttonPressed === "5"){
+		// 	result = 0;
+		// 	currentEntry = '5';
+		// } else if (buttonPressed === "6"){
+		// 	result = 0;
+		// 	currentEntry = '6';
+		// } else if (buttonPressed === "7"){
+		// 	result = 0;
+		// 	currentEntry = '7';
+		// } else if (buttonPressed === "8"){
+		// 	result = 0;
+		// 	currentEntry = '8';
+		// } else if (buttonPressed === "9"){
+		// 	result = 0;
+		// 	currentEntry = '9';
 		} else if (buttonPressed === '.') {
 			currentEntry += '.'
 		} else if (isNumber(buttonPressed)) {
